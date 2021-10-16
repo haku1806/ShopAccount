3fc1d20341d3d87f0453449a4b56a0187f7bce68
haku1806 <thangtranhtn2000@gmail.com>
2021-10-16 11:33:11 +0700

    Create project .net core

---
+++ ShopAccountCore/ShopAccountCore/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopAccountCore.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private IWebHostEnvironment _hostEnvironment;
+        
+        public HomeController(IWebHostEnvironment hostEnvironment)
+        {
+            this._hostEnvironment = hostEnvironment;
+        }
+
+        [Route("home")]
+        public String Get()
+        {
+            return "Api Ready";
+        }
+    }
+}
